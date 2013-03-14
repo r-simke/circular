5d8dba941941d87eabbcc821079fcbc202a8df3d
Rico Simke <rico.simke@adytonsystems.com>
2013-03-14 16:21:03 +0100

    side article does not validate a topic

--- app/models/sidearticle.rb
+++ app/models/sidearticle.rb
@@ -1,7 +1,6 @@
 class Sidearticle < ActiveRecord::Base
   belongs_to :newsletter
-  attr_accessible :content, :position, :topic, :newsletter
+  attr_accessible :content, :position, :topic, :newsletter_id
   acts_as_list scope: :newsletter
-  validates :topic, presence: true
   validates :newsletter, presence: true
 end
