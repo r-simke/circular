466e063cd59a68a70bc6bbf7288c9d1e2a069215
Rico Simke <rico.simke@adytonsystems.com>
2013-03-14 14:54:02 +0100

    newsletter, article: presence and dependency

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,4 +3,6 @@ class Article < ActiveRecord::Base
   belongs_to :antecessor, class_name:'Article'
   attr_accessible :content, :show_dividerline, :topic, :newsletter, :antecessor
   acts_as_list scope: :newsletter
+  validates :topic, presence: true
+  validates :newsletter, presence: true
 end
--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -1,4 +1,4 @@
 class Newsletter < ActiveRecord::Base
-  has_many :articles, order: "position"
+  has_many :articles, order: "position", dependent: :destroy
   attr_accessible :author_id, :code, :content, :show_dividerline, :show_outline, :status_id, :template_id, :topic
 end
