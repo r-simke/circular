7291d99ac17310be2cfbe3fbfcd79fe40fafa7fb
Rico Simke <rico.simke@adytonsystems.com>
2013-03-22 10:56:40 +0100

    article and sidearticle attributes accessible through newsletter model

--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -1,7 +1,9 @@
 class Newsletter < ActiveRecord::Base
   has_many :articles, order: "position", dependent: :destroy
   has_many :sidearticles, order: "position", dependent: :destroy
-  attr_accessible :author_id, :code, :content, :show_dividerline, :show_outline, :status_id, :template_id, :topic
+  attr_accessible :author_id, :code, :content, :show_dividerline, :show_outline, :status_id, :template_id, :topic, :articles_attributes, :sidearticles_attributes
   belongs_to :template
   belongs_to :author
+  accepts_nested_attributes_for :articles, allow_destroy: true
+  accepts_nested_attributes_for :sidearticles
 end
