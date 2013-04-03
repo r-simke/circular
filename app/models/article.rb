eaa883fec060e957543276521f15db3f19827992
Rico Simke <rico.simke@adytonsystems.com>
2013-04-03 12:31:36 +0200

    removed acts_as_list from models

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,5 @@
 class Article < ActiveRecord::Base
   belongs_to :newsletter
   attr_accessible :content, :position, :show_dividerline, :topic, :newsletter_id
-  acts_as_list scope: :newsletter
   validates :newsletter, presence: true
 end
--- app/models/sidearticle.rb
+++ app/models/sidearticle.rb
@@ -1,6 +1,5 @@
 class Sidearticle < ActiveRecord::Base
   belongs_to :newsletter
   attr_accessible :content, :position, :topic, :newsletter_id
-  acts_as_list scope: :newsletter
   validates :newsletter, presence: true
 end
