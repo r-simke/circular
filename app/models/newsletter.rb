d672ec104105f934152d989d918d13d7141becd5
Rico Simke <rico.simke@adytonsystems.com>
2013-04-03 12:40:46 +0200

    sidearticles can be destroyed via Newsletter

--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -1,9 +1,9 @@
 class Newsletter < ActiveRecord::Base
-  has_many :articles, order: "position", dependent: :destroy
-  has_many :sidearticles, order: "position", dependent: :destroy
+  has_many :articles, order: "position ASC", dependent: :destroy
+  has_many :sidearticles, order: "position ASC", dependent: :destroy
   attr_accessible :author_id, :code, :content, :show_dividerline, :show_outline, :status_id, :template_id, :topic, :articles_attributes, :sidearticles_attributes
   belongs_to :template
   belongs_to :author
   accepts_nested_attributes_for :articles, allow_destroy: true
-  accepts_nested_attributes_for :sidearticles
+  accepts_nested_attributes_for :sidearticles, allow_destroy: true
 end
