31345c4ddd05638c6ae598c212f423b703b0225f
Rico Simke <rico.simke@adytonsystems.com>
2013-04-04 15:39:05 +0200

    made seeds for default templates and default authors

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Template.create(name: "standard-english")
+Template.create(name: "standard-german")
+Template.create(name: "standard-english-only-ITLOGO")
+
+Author.create(
+	name: "Christiane Hultsch",
+	title: "Marketing Manager",
+	phone: "+49-341-392993430",
+	email: "pr@adytonsystems.com",
+	image_url: "http://www.adytonsystems.com/newsletter/img/contact-hultsch.jpg"
+	)
+
+Author.create(
+	name: "Romy Wuttke",
+	title: "Marketing Assistant",
+	phone: "+49-341-392993430",
+	email: "pr@adytonsystems.com",
+	image_url: "http://www.adytonsystems.com/newsletter/img/contact-wuttke.jpg"
+	)
+
+Author.create(
+	name: "Ralph Skoruppa",
+	title: "Head of Sales",
+	phone: "+49-341-392993430",
+	email: "sales@adytonsystems.com",
+	image_url: "http://www.adytonsystems.com/newsletter/img/contact-skoruppa.jpg"
+	)
