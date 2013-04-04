8973573960a248afd4385a33ab8edc5db1b3d625
Rico Simke <rico.simke@adytonsystems.com>
2013-04-04 10:51:49 +0200

    no "validates :newsletter" in (side)article model, because it raised an error when creating a new newsletter with (side)articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
   belongs_to :newsletter
   attr_accessible :content, :position, :show_dividerline, :topic, :newsletter_id
-  validates :newsletter, presence: true
+  # validates :newsletter, presence: true # raises error in nested forms
 end
--- app/models/sidearticle.rb
+++ app/models/sidearticle.rb
@@ -1,5 +1,5 @@
 class Sidearticle < ActiveRecord::Base
   belongs_to :newsletter
   attr_accessible :content, :position, :topic, :newsletter_id
-  validates :newsletter, presence: true
+  # validates :newsletter, presence: true # raises error in nested forms
 end
