e6118167049d8b2273cf4058131a9ac615ac10ea
Rico Simke <rico.simke@adytonsystems.com>
2013-04-09 09:13:32 +0200

    deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,6 @@ set :keep_releases, 3
 
 # if you want to clean up old releases on each deploy uncomment this:
 after "deploy:cold", "deploy:restart"
-after "deploy", "deploy:restart"
 after "deploy:restart", "deploy:seed"
 after "deploy:seed", "deploy:cleanup"
 
